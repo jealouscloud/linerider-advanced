ee70a0b6267d49cc5a7de71a561ed6630d12d1c8
jealouscloud <nablaseau@hotmail.com>
2018-02-21 17:45:51 -0500

    crash, startup changes

---
+++ src/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+namespace linerider
+{
+    public static class EntryPoint
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+#if DEBUG
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "debug")
+                {
+                    Program.IsDebugged = true;
+                }
+            }
+#endif
+            Program.Run();
+        }
+    }
+}
