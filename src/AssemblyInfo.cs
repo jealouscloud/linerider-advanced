b7583edebc0c6007d0afe480cdc01ad5e8350e54
jealouscloud <nablaseau@hotmail.com>
2018-02-25 16:01:57 -0500

    cleanup assemblyinfo

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -7,13 +7,6 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Line Rider: Advanced")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Noah Ablaseau")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
