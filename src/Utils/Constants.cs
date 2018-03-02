c8093880f6f1c926104b7f8b1317b98f84430c61
jealouscloud <nablaseau@hotmail.com>
2018-03-02 03:54:32 -0500

    nightmode is more gray

--- src/Utils/Constants.cs
+++ src/Utils/Constants.cs
@@ -7,7 +7,7 @@ namespace linerider.Utils
     {
         public static readonly Color4 ColorOffwhite = new Color4(244, 245, 249, 255);
         public static readonly Color4 ColorWhite = new Color4(255, 255, 255, 255);
-        public static readonly Color4 ColorNightMode = new Color4(20, 20, 25, 255);
+        public static readonly Color4 ColorNightMode = new Color4(22, 22, 22, 255);
         public static readonly int[] MotionArray =
         {
             1, 2, 5, 10, 20, 30, 40, 80, 160, 320, 640
