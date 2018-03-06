516a914065f22ff5438404cbfc54351e9dde5134
jealouscloud <nablaseau@hotmail.com>
2018-03-06 05:53:11 -0500

    remove empty using

--- src/Rendering/WellRenderer.cs
+++ src/Rendering/WellRenderer.cs
@@ -2,7 +2,6 @@ using OpenTK.Graphics.OpenGL;
 using System;
 using System.Drawing;
 using System.Collections.Generic;
-using System.Collections.Concurrent;
 using System.Runtime.InteropServices;
 using OpenTK;
 using linerider.Drawing;
