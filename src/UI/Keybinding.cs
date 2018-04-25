3a6d131ee8aa0b566a15dbd4e05e4b19d6a7219c
jealouscloud <nablaseau@hotmail.com>
2018-04-25 17:04:35 -0400

    Keybinding treats keys set with modifiers as just modifiers
    * Add ToString
    * Add equality comparison

--- src/UI/Keybinding.cs
+++ src/UI/Keybinding.cs
@@ -5,8 +5,39 @@ namespace linerider.UI
 {
     public class Keybinding
     {
+        private Key _key = (Key)(-1);
         public MouseButton MouseButton = (MouseButton)(-1);
-        public Key Key = (Key)(-1);
+        public Key Key
+        {
+            get
+            {
+                return _key;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case Key.AltLeft:
+                    case Key.AltRight:
+                        _key = (Key)(-1);
+                        Modifiers |= KeyModifiers.Alt;
+                        return;
+                    case Key.ShiftLeft:
+                    case Key.ShiftRight:
+                        _key = (Key)(-1);
+                        Modifiers |= KeyModifiers.Shift;
+                        return;
+                    case Key.ControlLeft:
+                    case Key.ControlRight:
+                        _key = (Key)(-1);
+                        Modifiers |= KeyModifiers.Control;
+                        return;
+                    default:
+                        _key = value;
+                        break;
+                }
+            }
+        }
         public KeyModifiers Modifiers = (KeyModifiers)(0);
         public bool IsEmpty => (Modifiers == (KeyModifiers)(0) && Key == (Key)(-1) && MouseButton == (MouseButton)(-1));
         public int KeysDown
@@ -29,16 +60,6 @@ namespace linerider.UI
         {
             get
             {
-                switch(Key)
-                {
-                    case Key.AltLeft:
-                    case Key.AltRight:
-                    case Key.ShiftLeft:
-                    case Key.ShiftRight:
-                    case Key.ControlLeft:
-                    case Key.ControlRight:
-                    return true;
-                }
                 return Modifiers != (KeyModifiers)(0);
             }
         }
@@ -59,16 +80,10 @@ namespace linerider.UI
         public Keybinding()
         {
         }
-        public Keybinding(MouseButton mouse, Key key, KeyModifiers modifiers)
-        {
-            Key = key;
-            MouseButton = mouse;
-            Modifiers = modifiers;
-        }
         public Keybinding(Key key, KeyModifiers modifiers)
         {
-            Key = key;
             Modifiers = modifiers;
+            Key = key;
         }
         public Keybinding(Key key)
         {
@@ -82,6 +97,125 @@ namespace linerider.UI
         {
             MouseButton = mouse;
             Modifiers = modifiers;
+        }
+        public bool IsBindingEqual(Keybinding other)
+        {
+            if (other == null)
+                return false;
+            return other.Key == Key && other.Modifiers == Modifiers && other.MouseButton == MouseButton;
+        }
+        public override string ToString()
+        {
+            if (IsEmpty)
+                return "Undefined";
+            string kb = "";
+            int modifiers = 0;
+            if (UsesModifiers)
+            {
+                if (Modifiers.HasFlag(KeyModifiers.Control))
+                {
+                    kb += "ctrl";
+                    modifiers++;
+                }
+                if (Modifiers.HasFlag(KeyModifiers.Shift))
+                {
+                    if (modifiers > 0)
+                    {
+                        kb += "+";
+                    }
+                    kb += "shift";
+                    modifiers++;
+                }
+                if (Modifiers.HasFlag(KeyModifiers.Alt))
+                {
+                    if (modifiers > 0)
+                    {
+                        kb += "+";
+                    }
+                    kb += "alt";
+                    modifiers++;
+                }
+            }
+            if (UsesKeys)
+            {
+                if (modifiers > 0)
+                    kb += "+";
+                kb += KeyToString(Key);
+            }
+            if (UsesMouse)
+            {
+
+                if (modifiers > 0)
+                    kb += "+";
+                kb += MouseButton;
+            }
+            return kb;
+        }
+        private string KeyToString(Key key)
+        {
+
+            switch (key)
+            {
+                case Key.Enter:
+                case Key.Escape:
+                case Key.Tab:
+                case Key.Space:
+                case Key.Up:
+                case Key.Down:
+                case Key.Left:
+                case Key.Right:
+                case Key.Home:
+                case Key.End:
+                case Key.Delete:
+                case Key.PageDown:
+                case Key.PageUp:
+                case Key.Insert:
+                    return key.ToString();
+                case Key.BackSpace: return "Backspace";
+                case Key.RControl:
+                case Key.LControl: return "Control";
+                case Key.RAlt:
+                case Key.LAlt: return "Alt";
+                case Key.RShift:
+                case Key.LShift: return "Shift";
+                case Key.BracketLeft:
+                    return "[";
+                case Key.BracketRight:
+                    return "]";
+                case Key.Semicolon:
+                    return ";";
+                case Key.Quote:
+                    return "\"";
+                case Key.Period:
+                    return ".";
+                case Key.Comma:
+                    return ",";
+                case Key.Grave:
+                    return "`";
+                case Key.Minus:
+                    return "-";
+                case Key.Plus:
+                    return "+";
+                case Key.Slash:
+                    return "/";
+                case Key.BackSlash:
+                    return "\\";
+                default:
+                    var trans = TranslateChar(key);
+                    if (trans == ' ')
+                        return key.ToString();//i give up
+                    return trans.ToString();
+            }
+        }
+        private static char TranslateChar(Key key)
+        {
+            if (key >= Key.A && key <= Key.Z)
+                return (char)('A' + ((int)key - (int)Key.A));
+            if (key >= Key.Number0 && key <= Key.Number9)
+                return (char)('0' + ((int)key - (int)Key.Number0));
+            if (key >= Key.Keypad0 && key <= Key.Keypad9)
+                return (char)('0' + ((int)key - (int)Key.Keypad0));
+            return ' ';
         }
     }
 }
